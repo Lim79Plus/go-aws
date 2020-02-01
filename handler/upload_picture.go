bbd62068cd69cd04d864dfb554dae08c72dc8f9a
Lim79Plus <masae.h79@gmail.com>
2020-02-01 16:34:19 +0900

    サーバ側でのアップロード機能の追加

--- handler/upload_picture.go
+++ handler/upload_picture.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,6 +12,32 @@ import (
 // UploadPicture 画像のアップロード
 func UploadPicture() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.String(http.StatusOK, "UploadPicture hello!!")
+		// Source
+		file, err := c.FormFile("uploadfile")
+		if err != nil {
+			return err
+		}
+		log.Printf("Uploaded file name %v", file.Filename)
+
+		// アップロードされたファイルを開ける
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+
+		// Destination os上にアップロードされたファイルと同名の空ファイルを作成する
+		dst, err := os.Create(file.Filename)
+		if err != nil {
+			return err
+		}
+		defer dst.Close()
+
+		// Copy
+		if _, err = io.Copy(dst, src); err != nil {
+			return err
+		}
+
+		return c.String(http.StatusOK, "UploadPicture successed:"+file.Filename)
 	}
 }
